3c540ea0b76173d4515e5b77ef1e0d33c52169c6
Muhammad-Sabbir <md.sabbirahamedtushar@gmail.com>
2023-04-05 19:16:18 +0600

    react ecommerce project progress

---
+++ src/homepage.component.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './homepage.styles.scss'
+const HomePage =()=>(
+    // we are using functional component because we don't need any lificycle components, no need to store any state.
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>HATS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>JACKETS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>SNEAKERS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>WEMENS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>MENS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default HomePage;
